1d874b0aed4dafd885f98954a6de5d42f06b0fe0
sumit <mytrade1086@gmail.com>
2020-08-30 12:19:38 +0530

    Clone Added

--- src/main/java/collectionsPrac/ArrayListDemo.java
+++ src/main/java/collectionsPrac/ArrayListDemo.java
@@ -1,5 +1,4 @@
 package collectionsPrac;
-
 import java.util.ArrayList;
 
 public class ArrayListDemo {
@@ -16,31 +15,26 @@ public class ArrayListDemo {
                                                   //note that only removes first occurence
         System.out.println(al); //[sumit]
         System.out.println(al.remove("hulk")); //false
-
-
-
         al.add(0,"NehaD");
         al.add(0,"Nehap");
 
         System.out.println("Shifts Original element downward "+al); //hence not good for middle insertions
         //Shifts Original element downward [Nehap, NehaD, sumit]
-        //
+
         System.out.println("Will return item originally present at index 2 "+al.set(2,"sagar"));//sumit
         //returns element which was previously present at this position
-
        // System.out.println("Will return item originally present at index 2 "+al.set(3,"sagar")); //java.lang.IndexOutOfBoundsException: Index: 3, Size: 3
         //set works with existing indexes only
 
         System.out.println(al);//[Nehap, NehaD, sagar]
-
         System.out.println(al.subList(0,3));//0 1 2     i.e  inclusive,exclusive [Nehap, NehaD, sagar]
 
-        al.clone()
+        ArrayList<String> demoClone= (ArrayList<String>) al.clone();
+        System.out.println(demoClone);//[Nehap, NehaD, sagar]
 
-
-
-
-
+        demoClone.set(0,"DNEHA");
+        System.out.println("From demoClone "+demoClone);//From demoClone [DNEHA, NehaD, sagar]
+        System.out.println("From al "+al);//From al [Nehap, NehaD, sagar]
 
 
     }
