40cae6727711f3c5198aed9e3cd9ac8c24dc3816
sumit <mytrade1086@gmail.com>
2020-08-31 13:38:33 +0530

    Added New Code

--- src/main/java/MapInterface/HashMapDemo.java
+++ src/main/java/MapInterface/HashMapDemo.java
@@ -5,18 +5,15 @@ import java.util.Map;
 import java.util.Set;
 
 public class HashMapDemo {
-
     public static void main(String[] args) {
-
-        HashMap<String,Integer> hm=new HashMap<>();
+        Map<String,Integer> hm=new HashMap<>();
         System.out.println(hm.isEmpty());//true
         System.out.println(hm.put("Sumit",1) );//returns null as sumit never had mapping
-        System.out.println(hm.put("Sumit",11) );// ***returns old mapping
-        System.out.println(hm.put("Amit",2) );
+        System.out.println(hm.put("Sumit",11) );// ***returns old mapping 1 i this case
+        System.out.println(hm.put("Amit",2) );// null as first time
 
         System.out.println(hm.get("Sumit")); //11
         System.out.println(hm.isEmpty());//false
-
         System.out.println("Size is "+hm.size());//1
         Set<String> setKeys=hm.keySet();
 
@@ -33,9 +30,19 @@ public class HashMapDemo {
 //            Used entrySet() :: Key is Sumit Value is :11
         }
 
+        hm.forEach((k,v)-> System.out.println("k and value "+ k+" ==>"+v));
+//        k and value Amit ==>2
+//        k and value Sumit ==>11
 
+        System.out.println(hm.replace("Sumit",111));//old value 11 returned
+        System.out.println(hm);//{Amit=2, Sumit=111}
+        System.out.println(hm.containsKey("Sumit"));//true
+        System.out.println(hm.containsValue(11));//false
 
+   hm.compute("Sumit",(k,v)->(v==null)?1:v+1);
+        System.out.println(hm);//{Amit=2, Sumit=112}
 
+        hm.co
 
     }
 }
--- src/main/java/MapInterface/TreeMapDemo.java
+++ src/main/java/MapInterface/TreeMapDemo.java
@@ -1,4 +1,32 @@
 package MapInterface;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class TreeMapDemo {
+
+    public static void main(String[] args) {
+        Map<String,Integer> tm=new TreeMap<String,Integer>();
+        tm.put("Maths",35);
+        tm.put("Physics",65);
+        tm.put("English",75);
+        tm.put("Algorith",99);
+        System.out.println(tm);//{Algorith=99, English=75, Maths=35, Physics=65}
+        //Sorted as per Key
+
+
+
+        Map<StringBuffer,Integer> tmsb=new TreeMap<StringBuffer,Integer>();
+//        tmsb.put("A",10);  //Wont allow String Buffer in tree map
+//        tmsb.put("Z",10);
+//        tmsb.put("C",10);
+
+
+
+
+
+
+
+
+    }
 }
